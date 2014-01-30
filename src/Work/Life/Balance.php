79371d0a624cb829d43aa10dcc5533cb15bf0f15
Barney Hanlon <bhanlon@inviqa.com>
2014-01-30 10:12:35 +0000

    Added levels of client urgency as constants

--- src/Work/Life/Balance.php
+++ src/Work/Life/Balance.php
@@ -7,13 +7,25 @@ use \Work\Life\PersonalLife;
 
 class Balance
 {
-    public function areMyChildrenMoreImportantThan(Client $client, $urgency = 'CRITICAL')
-    {
+    const URGENCY_CRITICAL = 'CRITICAL';
+
+    const URGENCY_EMERGENCY = 'OMG THIS IS AN EMERGENCY!';
+
+    const URGENCY_VITAL = 'VITAL';
+
+    const URGENCY_URGENT = 'URGENT';
+
+    public function areMyChildrenMoreImportantThan(
+        Client $client,
+        $urgency = self::URGENCY_CRITICAL
+    ) {
         return true;
     }
 
-    public function isMyHealthMoreImportantThan(Client $client, $urgency = 'CRITICAL')
-    {
+    public function isMyHealthMoreImportantThan(
+        Client $client,
+        $urgency = self::URGENCY_CRITICAL
+    ) {
         return true;
     }
 
